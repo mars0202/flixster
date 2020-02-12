7681a8286dd9aff4b048504332058c04d259849d
Manish Rajendran <rajend16@msu.edu>
2020-02-12 18:38:49 -0500

    Complected collection view

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Manish Rajendran on 2/12/20.
+//  Copyright © 2020 Manish Rajendran. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
